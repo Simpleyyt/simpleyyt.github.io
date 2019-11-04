db25b4ebbe521d128a51f26f3d9865d08c353e9c
yinlidong77 <yldyyn@163.com>
2019-11-04 15:53:49 +0800

    tj

--- assets/js/src/motion.js
+++ assets/js/src/motion.js
@@ -282,7 +282,8 @@ $(document).ready(function () {
 
     sidebar: function (integrator) {
       if (CONFIG.sidebar.display === 'always') {
-          NexT.utils.displaySidebar();
+          // NexT.utils.displaySidebar();
+          this.showSidebar();
           // sidebarToggleMotion.clickHandler();
       }
       integrator.next();
